136595acebf309fd6e6a406ce4428f501e7a74f5
ojuniorbezerra <junio_vjc@hotmail.com>
2020-06-04 11:26:28 -0300

    Implement movi snake

--- snake/game.js
+++ snake/game.js
@@ -19,5 +19,6 @@ function update(){
 }
 
 function draw(){
+    gambeBoard.innerHTML = ""
     drawSnake(gambeBoard)
 }
--- snake/snake.js
+++ snake/snake.js
@@ -1,19 +1,21 @@
 export const SNAKE_SPEED = 1
-
-const snakeBody = [{x: 10, y: 11},
-                    {x: 11, y: 11},
-                    {x: 12, y: 11}
-                ]
+ import { getInputDirection } from './input.js'
+const snakeBody = [{x: 10, y: 11}]
 
 export function update(){
-    console.log('update snake')
+    const inputDirection = getInputDirection()
+    for (let i = snakeBody.length -2 ; i >= 0; i--){
+        snakeBody[i+1] = {...snakeBody[i] }
+    }
+    snakeBody[0].x += inputDirection.x
+    snakeBody[0].y += inputDirection.y
 }
 
 export function draw(gameBoard){
  snakeBody.forEach( segment => {
     const snakeElement = document.createElement('div')
-    snakeElement.style.gridRowStart = segment.x
-    snakeElement.style.gridColumnStart = segment.y
+    snakeElement.style.gridRowStart = segment.y
+    snakeElement.style.gridColumnStart = segment.x
     snakeElement.classList.add('snake')
     gameBoard.appendChild(snakeElement)
  })
